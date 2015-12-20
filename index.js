ce61f912dccf3dc9cd4d0e3c06442b37eb7ec2f2
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2015-12-19 23:17:58 -0300

    Heroku keep awake

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 require("coffee-script/register");
 
 require("./src/server")
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://sorteo-wimbledon.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
